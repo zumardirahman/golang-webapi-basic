f342d8d968c888c13cd3a29a7f0831d7012daa20
zumardirahman <zumar2014@gmail.com>
2022-02-28 16:37:07 +0700

    Mengambil data Database

--- main.go
+++ main.go
@@ -27,19 +27,38 @@ func main() {
 	db.AutoMigrate(&book.Book{})
 	//CRUD
 
-	book := book.Book{}
-	book.Title = "Cari Pengalaman"
-	book.Price = 90000
-	book.Discount = 10
-	book.Rating = 5
-	book.Description = "ini adalah buku yang sangat bagus dari zumardi rahman"
+	// book := book.Book{}
+	// book.Title = "Cari Pengalaman"
+	// book.Price = 90000
+	// book.Discount = 10
+	// book.Rating = 5
+	// book.Description = "ini adalah buku yang sangat bagus dari zumardi rahman"
 
-	err = db.Create(&book).Error
+	// err = db.Create(&book).Error
+
+	// if err != nil {
+	// 	fmt.Println("================================")
+	// 	fmt.Println("Error vreating book record")
+	// 	fmt.Println("================================")
+	// }
+
+	//	var book book.Book
+	var books []book.Book
+
+	//err = db.Debug().First(&book,2).Error //mengambil primary key
+	// err = db.Debug().Find(&books).Error //mengambil bnyak data
+	err = db.Debug().Where("rating = ?", 5).Find(&books).Error //mengambil wehere
 
 	if err != nil {
 		fmt.Println("================================")
-		fmt.Println("Error vreating book record")
+		fmt.Println("Error Finding book record")
 		fmt.Println("================================")
+	}
+
+	for _, b := range books {
+
+		fmt.Println("title:", b.Title)
+		fmt.Println("book object", b)
 	}
 
 	router := gin.Default()
