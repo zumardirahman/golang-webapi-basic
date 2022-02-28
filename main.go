87d8035c9764b136ac16c0fdec2b486f132fec21
zumardirahman <zumar2014@gmail.com>
2022-02-28 11:51:18 +0700

    Path Variable dan Query String

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler) // :id akan dapat berubah
+	router.GET("/query", queryHandler)            // ex ?id=232
 
 	router.Run(":8888")
 }
@@ -26,5 +27,23 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":    "Hello World",
 		"subtitle": "My Golang Basic",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id") //parameter
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title") //query string
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
